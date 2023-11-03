e40fce765473e081ce424d3e4c607797e0d341c5
Igor Rakush <irakush@aol.com>
2023-11-03 12:34:08 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let newArray = [];
+
+function writeCards(array, text){
+  for (let index = 0; index < array.length; index++) {
+    // const element = array[index];
+    newArray[index] = `Thank you, ${array[index]}, for the wonderful ${text} gift!`;
+  };
+  return newArray;
+};
+
+function countDown(num){
+  let count = 0;
+  while (count <= num) {
+    console.log(count++);
+  };
+};
